52616356c6bd1fa0bc8db594adaa107b49950414
AshikShuvo <ashikshuvo1996@gmail.com>
2020-06-02 21:23:08 +0600

    checking all are working

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="jumbotron">
-    <h1 class="display-3">Welcome to LSAPP</h1>
-    <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, laudantium? Rem ipsum earum cupiditate aliquid nam accusantium eligendi vel a.</p>
-    <hr class="my-2">
-    <p class="lead">
-        <a class="btn btn-success btn-lg" href="Jumbo action link" role="button">Login</a>
-    </p>
+<div class="container">
 
 </div>
 

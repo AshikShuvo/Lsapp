60d8981b01ee76a72b2c73a04d57852882417cba
AshikShuvo <ashikshuvo1996@gmail.com>
2020-06-03 07:07:56 +0600

    overlay

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,48 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-
-</div>
-
+<header>
+    <div class="dark-overlay">
+        <div class="home-inner">
+            <div class="container row">
+                <div class="col-lg-8">
+                    <div class="display-4">
+                       <strong> Manage</strong> Your <strong>Business</strong> And Grow your <strong> profit</strong>
+                    </div>
+                    <div class="d-flex flex-row">
+                       <div class="p-4 align-self-start">
+                        <i style="color: black;" class="fa fa-check"></i>
+                       </div>
+                       <div class="align-self-end">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, earum consectetur odio quis maxime in laudantium laborum. Deserunt, similique nobis?
+                       </div>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="p-4 align-self-start">
+                         <i style="color: black;" class="fa fa-check"></i>
+                        </div>
+                        <div class="align-self-end">
+                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, earum consectetur odio quis maxime in laudantium laborum. Deserunt, similique nobis?
+                        </div>
+                     </div>
+                     <div class="d-flex flex-row">
+                        <div class="p-4 align-self-start">
+                         <i style="color: black;" class="fa fa-check"></i>
+                        </div>
+                        <div class="align-self-end">
+                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, earum consectetur odio quis maxime in laudantium laborum. Deserunt, similique nobis?
+                        </div>
+                     </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card border-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Features</div>
+                        <div class="card-body text-primary">
+                          <h5 class="card-title">Our offerings</h5>
+                          <p class="card-text text-center text-success">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias distinctio soluta, similique obcaecati natus eveniet inventore laudantium iste facere a nobis repellendus porro. Omnis quidem consequatur placeat iure officia dignissimos?</p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</header>
 @endsection
